0799ef19222bd81f7e4c3aa86c1efca520f7667b
Alan <alancravey@gmail.com>
2016-07-09 17:23:54 -0400

    Add files via upload

---
+++ javascript/quiz.js
@@ -0,0 +1,14 @@
+function checkQues() {
+	var ques = document.getElementsByName("q1");
+	var numOfQues = ques.length;
+
+	for(var i = 0; i < numOfQues; i++) {
+		if(ques[i].checked) {
+			if(ques[i].value=="Correct")
+				quiz.resultArea = "Correct!";
+			else
+				quiz.resultArea = "Sorry!";
+			
+		}
+	}
+}
